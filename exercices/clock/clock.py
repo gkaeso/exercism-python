54c27e56d6474124a92d0aa3bd6aa93a4f44afa2
Guillaume Marcel <guillaume.k.marcel@gmail.com>
2021-05-10 15:28:56 +0200

    solved - clock

---
+++ exercices/clock/clock.py
@@ -0,0 +1,20 @@
+class Clock:
+    def __init__(self, hour, minute):
+        self.total: int = hour * 60 + minute    # in minutes
+
+    def __repr__(self):
+        hour, minute = (self.total // 60) % 24, self.total % 60
+        return '{:02d}:{:02d}'.format(hour, minute)
+
+    def __eq__(self, other):
+        return repr(self) == repr(other)
+
+    def __add__(self, minutes: int):
+        self.total += minutes
+        return self
+
+    def __sub__(self, minutes: int):
+        self.total -= minutes
+        return self
+
+print(Clock(10,3) - 30)
